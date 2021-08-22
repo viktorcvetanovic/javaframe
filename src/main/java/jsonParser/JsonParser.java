1371078e11e81fefa6b78ab24455155d4fec01a3
Viktorrrrr <viktorcvetanovic@gmail.com>
2021-08-22 21:46:34 +0200

    added json parser for list

--- src/main/java/jsonParser/JsonParser.java
+++ src/main/java/jsonParser/JsonParser.java
@@ -15,36 +15,92 @@ public class JsonParser implements JsonParserInterface {
     @Override
     public Map<Object, Object> parseJson(@NonNull String json) {
         this.jsonString = json.trim();
-        List<Object> list=new ArrayList<>();
-        StringBuilder keyBuilder = new StringBuilder();
-        StringBuilder valueBuilder = new StringBuilder();
+
         var counter = 0;
-        boolean keyOrValue = true;
+
         if (this.jsonString.charAt(0) != '{' || jsonString.charAt(jsonString.length() - 1) != '}') {
-            throw new InvalidJsonFormatException("Invalid JSON format, please check it");
+            throw new InvalidJsonFormatException("Your Json is not valid");
         }
         while (true) {
             if (counter == jsonString.length()) {
                 break;
             }
             var current = jsonString.charAt(counter++);
-            if (current == ' ' || current == '"' || current == '{' || current == '\n') {
+            if (current == ' ' || current == '"' || current == '\n') {
                 continue;
             }
-            if(current=='['){
+            if (current == '{') {
+                readObject(counter);
             }
-            if(current==']'){
+
+            if (current == '[') {
+                readArray(counter);
+            }
+//                throw new InvalidJsonFormatException(String.format("You have error in your Json %s", counter));
+
+        }
+        return map;
+    }
+
+    private void readArray(int counter) {
+        List<Object> list = new ArrayList<>();
+        StringBuilder keyBuilder = new StringBuilder();
+        while (true) {
+            var current = jsonString.charAt(counter++);
+
+            if (current == '"') {
+                continue;
+            }
+
+            if (current == '{') {
+                readObject(counter);
+            }
+            if (current == ',') {
+                list.add(keyBuilder.toString());
+                keyBuilder.setLength(0);
+                continue;
+            }
+            if (current == ']') {
+                map.put("lista", list);
+                break;
+            }
+            keyBuilder.append(current);
+        }
+    }
+
+
+    private void readObject(int counter) {
+
+        StringBuilder keyBuilder = new StringBuilder();
+        StringBuilder valueBuilder = new StringBuilder();
+        boolean keyOrValue = true;
+        while (true) {
+            var current = jsonString.charAt(counter++);
+
+            if (current == '"') {
+                continue;
             }
             if (current == ':') {
                 keyOrValue = false;
                 continue;
             }
-            if (current == ',' || current == '}') {
+
+            if (current == ',') {
                 keyOrValue = true;
                 map.put(keyBuilder.toString(), valueBuilder.toString());
                 keyBuilder.setLength(0);
                 valueBuilder.setLength(0);
                 continue;
+            }
+            if (current == '}') {
+                keyOrValue = true;
+                map.put(keyBuilder.toString(), valueBuilder.toString());
+                keyBuilder.setLength(0);
+                valueBuilder.setLength(0);
+                break;
+            }
+            if (current == '{') {
+                readObject(counter);
             }
             if (keyOrValue) {
                 keyBuilder.append(current);
@@ -52,14 +108,7 @@ public class JsonParser implements JsonParserInterface {
                 valueBuilder.append(current);
             }
         }
-        return map;
     }
-
-    private void readObject(){
-
-    }
-
-
 
 
 }
